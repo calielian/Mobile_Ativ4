bafae06a0037a3eb32b2cd26a1fccde356055cb1
calielian <calielian@proton.me>
2026-05-15 08:25:42 -0300

    68

--- app/src/main/java/com/calielian/task/ui/auth/LoginFragment.kt
+++ app/src/main/java/com/calielian/task/ui/auth/LoginFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.calielian.task.R
 import com.calielian.task.databinding.FragmentLoginBinding
 import com.calielian.task.databinding.FragmentSplashBinding
@@ -19,6 +20,16 @@ class LoginFragment : Fragment() {
     ): View {
         _binding = FragmentLoginBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    private fun initListener() {
+        binding.btnRegister.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+        }
+
+        binding.btnRegister.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_recoverAccountFragment)
+        }
     }
 
     override fun onDestroyView() {
